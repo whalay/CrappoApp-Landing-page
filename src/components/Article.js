5068bef976acc1c400f754000c9411f2bfdc9f30
soliu yusuf <soliuy5@gmail.com>
2022-09-07 17:34:41 +0100

    l

--- src/components/Article.js
+++ src/components/Article.js
@@ -12,8 +12,8 @@ const Article = () => {
                         Porro, id similique temporibus dignissimos tempore odio?</p>
                 </div>
                 <div data-aos="zoom-in" data-aos-delay="350" data-aos-duration="2000" data-aos-anchor-placement="bottom-bottom">
-                    <div className='text-black bg-white p-6 mx-auto  md:w-[80%] rounded-3xl '>
-                        <form action="" className='md:flex justify-between gap-16 items-center p-1  '>
+                    <div className='text-black bg-white p-2 mx-auto  md:w-[80%] rounded-3xl '>
+                        <form action="" className='md:flex justify-between gap-16 items-center p-3  '>
                             <input type="text" name="" id="" placeholder='Enter your hash rate'  className='border-b p-2 mb-5 text-xl md:text-xl outline-none w-full' />
                             <select name="list" id="" form="list"  className='border-b mb-5 p-2 block text-xl outline-none md:w-52  w-full'>
                                 <option selected value="THS">TH/S</option>
@@ -24,7 +24,7 @@ const Article = () => {
                             </select>
                             <button className='px-9 py-5 rounded-full bg-blue-500 block font-bold text-white'>Calculate</button>
                         </form>
-                        <div className='px-6'>
+                        <div className='px-3'>
                             <h3 className='text-xl py-5 text-blue-500'>ESTIMATED 24 HOUR REVENUE:</h3>
                             <h2 className=' text-3xl font-bold '>0.055 130 59 ETH <span className="text-blue-500">($1275)</span></h2>
                             <p className='py-5 text-md'>Revenue will change based on mining difficulty and Ethereum price.</p>
